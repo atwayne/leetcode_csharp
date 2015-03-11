f84237c0dfbb7d827d3b636ed477a6f000c4de5e
Wayne Wang <lastr2d2@gmail.com>
2015-03-12 04:52:00 +0800

    solve 190

---
+++ Solutions/Solutions.Tests/Problems.Tests/Problem190Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problems;
+
+namespace Solutions.Tests.Problems.Tests
+{
+    [TestClass]
+    public class Problem190Test
+    {
+        [TestMethod]
+        public void ReverseBits()
+        {
+            var data = new Dictionary<uint, uint>
+            {
+                {43261596,964176192}
+            };
+
+            foreach (var pair in data)
+            {
+                var input = pair.Key;
+                var expected = data[input];
+                var actual = Problem190.ReverseBits(input);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem190.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reverse-bits/
+    /// 
+    /// Reverse bits of a given 32 bits unsigned integer.
+    /// For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+    /// Follow up:
+    /// If this function is called many times, how would you optimize it?
+    /// </summary>
+    public class Problem190
+    {
+        public static uint ReverseBits(uint n)
+        {
+            uint result = 0;
+            var count = 0;
+            while (n > 0)
+            {
+                result = result * 2 + n % 2;
+                n = n >> 1;
+                count++;
+            }
+            return result << (32 - count);
+        }
+    }
+}
