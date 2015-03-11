4db585fbe9b236ae76cba0ca9fcd34051ab3c52e
Wayne Wang <lastr2d2@gmail.com>
2015-03-12 04:31:43 +0800

    create solution and solve 191

---
+++ Solutions/Solutions.Tests/Problems.Tests/Problem191Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problems;
+
+namespace Solutions.Tests.Problems.Tests
+{
+    [TestClass]
+    public class Problem191Test
+    {
+        [TestMethod]
+        public void HammingWeightTests()
+        {
+            var data = new Dictionary<uint, int>
+            {
+                {1,1},
+                {2,1},
+                {3,2},
+                {11,3},
+                {12,2}
+            };
+
+            foreach (var pair in data)
+            {
+                var input = pair.Key;
+                var expected = data[input];
+                var actual = Problem191.HammingWeight(input);
+                Assert.AreEqual(actual, expected);
+            }
+        }
+    }
+}
