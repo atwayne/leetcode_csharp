0deef1b132680d4ff757f20ad17ec14c794104d5
Wayne Wang <lastr2d2@gmail.com>
2015-03-12 19:31:37 +0800

    solve 189

---
+++ Solutions/Solutions.Tests/Problems.Tests/Problem189Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problems;
+
+namespace Solutions.Tests.Problems.Tests
+{
+    [TestClass]
+    public class Problem189Test
+    {
+        [TestMethod]
+        public void RotateTest()
+        {
+            var testcases = new List<TestCase>
+            {
+                new TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 },3,new int[] { 5, 6, 7, 1, 2, 3, 4 }),
+                new TestCase(new int[] { 1, 2 },0,new int[] { 1,2 }),
+                new TestCase(new int[] { 1, 2 },3,new int[] { 2,1 })
+            };
+
+            foreach (var testcase in testcases)
+            {
+                var input = testcase.input;
+                var rotate = testcase.rotate;
+                var expected = testcase.expected;
+                Problem189.Rotate(input, rotate);
+                CollectionAssert.AreEqual(expected, input);
+            }
+        }
+
+        private class TestCase
+        {
+            public int[] input;
+            public int rotate;
+            public int[] expected;
+
+            public TestCase(int[] input, int rotate, int[] expected)
+            {
+                this.input = input;
+                this.rotate = rotate;
+                this.expected = expected;
+            }
+        }
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem189.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/rotate-array/
+    /// 
+    /// 
+    /// Rotate an array of n elements to the right by k steps.
+    /// For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+    /// </summary>
+    public class Problem189
+    {
+        public static void Rotate(int[] nums, int k)
+        {
+            var length = nums.Length;
+
+            k = k % length;
+            if (k < 0) k += length;
+
+            if (k == 0)
+                return;
+
+
+            
+            var result = new int[length];
+            var shift = length - k;
+
+            for (var i = 0; i < length; i++)
+            {
+                result[i] = nums[(shift + i) % length];
+            }
+
+            for (var i = 0; i < length; i++)
+            {
+                nums[i] = result[i];
+            }
+        }
+    }
+}
