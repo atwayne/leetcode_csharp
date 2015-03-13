41e892c37734baf2c45438b3b9297d909573088f
Wayne Wang <lastr2d2@gmail.com>
2015-03-13 12:34:34 +0800

    adding 046 problem

---
+++ Solutions/Solutions.Tests/Problems.Tests/Problem046Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problems;
+
+namespace Solutions.Tests.Problems.Tests
+{
+    [TestClass]
+    public class Problem046Test
+    {
+        [TestMethod]
+        public void PermuteTest()
+        {
+            var input = new[] { 1, 2, 3 };
+            var expected = new List<IList<int>>
+            {
+                new []{1,2,3},
+                new []{1,3,2},
+                new []{2,1,3},
+                new []{2,3,1},
+                new []{3,1,2},
+                new []{3,2,1},
+            };
+            var actual = Problem046.Permute(input);
+            Assert.IsNotNull(actual);
+            // compare count first before I think about another solution
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem046.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/permutations/
+    /// 
+    /// Given a collection of numbers, return all possible permutations.
+    /// For example,
+    /// [1,2,3] have the following permutations:
+    /// [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+    /// </summary>
+    public class Problem046
+    {
+        public static IList<IList<int>> Permute(int[] num)
+        {
+            var result = new List<IList<int>>();
+
+            //var 
+
+            return result;
+        }
+
+        public static IList<int> Next(int[] num)
+        {
+            var i = 0;
+            var length = num.Length;
+            while (i < length - 2 && num[i] < num[i + 1])
+            {
+                i++;
+            }
+
+            if (num[i] < num[i + 1])
+                return null;
+
+
+
+
+
+        }
+
+        public static IList<int> Prev(int[] num) { }
+    }
+}
