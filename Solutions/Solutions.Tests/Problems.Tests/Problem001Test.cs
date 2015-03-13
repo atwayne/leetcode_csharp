bfd0ef0b67a81b18f1091ff2e22d99784ae0440c
Wayne Wang <lastr2d2@gmail.com>
2015-03-14 02:10:25 +0800

    solve 001
    should improve performance. Time Limit Exceeded on leetcode

---
+++ Solutions/Solutions.Tests/Problems.Tests/Problem001Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problems;
+
+namespace Solutions.Tests.Problems.Tests
+{
+    [TestClass]
+    public class Problem001Test
+    {
+        [TestMethod]
+        public void TwoSumTest()
+        {
+            var numbers = new [] { 2, 7, 11, 15 };
+            var target = 9;
+
+            var actual = Problem001.TwoSum(numbers, target);
+            Assert.AreEqual(actual.Item1, 1);
+            Assert.AreEqual(actual.Item2, 2);
+        }
+    }
+}
