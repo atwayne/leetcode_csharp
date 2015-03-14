905a6bb5a4e404475ad1d6231e301559a2468c62
Wayne Wang <lastr2d2@gmail.com>
2015-03-15 07:22:28 +0800

    solve 008

---
+++ Solutions/Solutions.NUnitTests/Tests/Problem008Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Solutions.Problems;
+
+namespace Solutions.NUnitTests.Tests
+{
+    [TestFixture]
+    public class Problem008Tests
+    {
+        [Test]
+        public void ReverseTest()
+        {
+            var testcases = new Dictionary<int, int>
+            {
+                {-1,-1},
+                {123,321},
+                {-98765,-56789},
+                {2147483647,0},
+                {-2147483648,0},
+                {2147447412,2147447412},
+                {-2147447412,-2147447412},
+                {0,0}
+            };
+
+            foreach (var testcase in testcases)
+            {
+                var input = testcase.Key;
+                var expected = testcase.Value;
+                var actual = Problem008.Reverse(input);
+                Assert.AreEqual(expected, actual);
+
+            }
+        }
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem008.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reverse-integer/
+    /// 
+    /// Reverse digits of an integer.
+    /// Example1: x = 123, return 321
+    /// Example2: x = -123, return -321
+    /// 
+    /// </summary>
+    public class Problem008
+    {
+        public static int Reverse(int x)
+        {
+            if (x == 0)
+                return 0;
+
+            var result = 0;
+            var overflow = 214748364;
+
+            while (x != 0)
+            {
+                var remainder = x % 10;
+                if ((x > 0 && result > overflow)
+                    || (x < 0 && result < -overflow))
+                    return 0;
+                result = result * 10 + remainder;
+                x /= 10;
+            }
+
+            return result;
+        }
+    }
+}
