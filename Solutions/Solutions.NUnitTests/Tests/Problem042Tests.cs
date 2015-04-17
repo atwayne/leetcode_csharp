3617fda0cf9d50cabf4d55656146a14de6e20c35
Wayne Wang <lastr2d2@gmail.com>
2015-04-17 20:06:46 +0800

    adding 042

---
+++ Solutions/Solutions.NUnitTests/Tests/Problem042Tests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Solutions.Problems;
+
+namespace Solutions.NUnitTests.Tests
+{
+    [TestFixture]
+    public class Problem042Tests
+    {
+        [Test]
+        public void TrapTestOfficalExampleShouldPass()
+        {
+            var input = new List<int> { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            var actual = Problem042.Trap(input);
+            var expected = 6;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void TrapTestAllSameHeightShouldPass()
+        {
+            var input = new List<int> { 1, 1, 1, 1, 1, 1 };
+            var actual = Problem042.Trap(input);
+            var expected = 0;
+            Assert.AreEqual(actual, expected);
+        }
+
+
+        [Test]
+        public void TrapTestSortedArrayShouldPass()
+        {
+            var input = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var actual = Problem042.Trap(input);
+            var expected = 0;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void TrapTestReversedSortedArrayShouldPass()
+        {
+            var input = new List<int> { 6, 5, 4, 3, 2, 1 };
+            var actual = Problem042.Trap(input);
+            var expected = 0;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void TrapTestMonutainShapeArrayShouldPass()
+        {
+            var input = new List<int> { 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1 };
+            var actual = Problem042.Trap(input);
+            var expected = 0;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void TrapTestValleryArrayShouldPass()
+        {
+            var input = new List<int> { 4, 2, 2, 3 };
+            var actual = Problem042.Trap(input);
+            var expected = 2;
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem042.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/trapping-rain-water/
+    /// 
+    /// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+    /// For example, 
+    /// Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+    /// </summary>
+    public class Problem042
+    {
+        public static int Trap(IList<int> height)
+        {
+            // we need at least three elements
+            //if (height.Count < 3)
+            //    return 0;
+
+            //var max = height.Count;
+
+            //var peaks = new List<int>();
+            //var left = 0;
+            //var next = left;
+            //while(height[next])
+        }
+    }
+}
