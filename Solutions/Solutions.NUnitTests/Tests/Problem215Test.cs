f1e30a29546a8512d065f80ebda9146fe1ba5e94
Wayne Wang <lastr2d2@gmail.com>
2015-06-29 18:13:47 +0800

    215

---
+++ Solutions/Solutions.NUnitTests/Tests/Problem215Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Solutions.Problems;
+
+namespace Solutions.NUnitTests.Tests
+{
+    [TestFixture()]
+    public class Problem215Test
+    {
+        [Test()]
+        public void FindKthLargestTest()
+        {
+            var input = new int[] { 3, 2, 1, 5, 6, 4 };
+            var k = 2;
+            var actual = Problem215.FindKthLargest(input, k);
+            var expected = 5;
+            Assert.AreEqual(actual, expected);
+
+            input = new int[] { -1, -1 };
+            k = 2;
+            actual = Problem215.FindKthLargest(input, k);
+            expected = -1;
+            Assert.AreEqual(actual, expected);
+
+
+            input = new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
+            k = 9;
+            actual = Problem215.FindKthLargest(input, k);
+            expected = 1;
+            Assert.AreEqual(actual, expected);
+
+            input = new int[] { 2, 1 };
+            k = 1;
+            actual = Problem215.FindKthLargest(input, k);
+            expected = 2;
+            Assert.AreEqual(actual, expected);
+        }
+
+
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem215.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/largest-number/
+    /// 
+    /// Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+    /// 
+    /// For example,
+    /// Given [3,2,1,5,6,4] and k = 2, return 5.
+    /// 
+    /// </summary>
+    public class Problem215
+    {
+        public static int FindKthLargest(int[] nums, int k)
+        {
+            var pivot = nums[nums.Length / 2];
+
+            var smaller = new List<int>();
+            var greater = new List<int>();
+            var count = 0;
+
+            var index = 0;
+            while (index < nums.Length)
+            {
+                if (nums[index] < pivot)
+                {
+                    smaller.Add(nums[index]);
+                }
+                else if (nums[index] > pivot)
+                {
+                    greater.Add(nums[index]);
+                }
+                else
+                {
+                    count++;
+                }
+                index++;
+            }
+
+
+            if (greater.Count >= k)
+            {
+                return FindKthLargest(greater.ToArray(), k);
+            }
+
+            if (greater.Count == k - 1 || greater.Count + count >= k)
+            {
+                return pivot;
+            }
+
+            return FindKthLargest(smaller.ToArray(), k - greater.Count - count);
+        }
+    }
+}
