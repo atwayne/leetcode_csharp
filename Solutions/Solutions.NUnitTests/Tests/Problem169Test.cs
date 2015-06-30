08b442081d42816ca6b5d71d414c1d6ccb90403c
Wayne Wang <lastr2d2@gmail.com>
2015-06-30 14:02:31 +0800

    169

---
+++ Solutions/Solutions.NUnitTests/Tests/Problem169Test.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Solutions.Problems;
+
+namespace Solutions.NUnitTests.Tests
+{
+    [TestFixture()]
+    public class Problem169Test
+    {
+        [Test()]
+        public void MajorityElementTest()
+        {
+            var input = new int[] { 1 };
+            var expected = 1;
+            var actual = Problem169.MajorityElement(input);
+
+            Assert.AreEqual(expected, actual);
+
+
+            input = new int[] { 1, 2, 3, 4, 5, 3, 3, 3, 3, 3 };
+            expected = 3;
+            actual = Problem169.MajorityElement(input);
+            Assert.AreEqual(expected, actual);
+
+
+            input = new int[] { 3, 3, 3, 3, 4, 4, 4 };
+            expected = 3;
+            actual = Problem169.MajorityElement(input);
+            Assert.AreEqual(expected, actual);
+
+
+            input = new int[] { 2, 2, 2, 3, 3, 3, 3 };
+            expected = 3;
+            actual = Problem169.MajorityElement(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem169.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/majority-element/
+    /// 
+    /// Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+    /// You may assume that the array is non-empty and the majority element always exist in the array.
+    /// 
+    /// </summary>
+    public class Problem169
+    {
+        public static int MajorityElement(int[] nums)
+        {
+            var n = nums.Length;
+
+            var bitmap = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var key = nums[i];
+                if (bitmap.ContainsKey(key))
+                {
+                    bitmap[key]++;
+                }
+                else
+                {
+                    bitmap[key] = 1;
+                }
+
+                if (bitmap[key] > nums.Length / 2)
+
+                    if (bitmap[key] > n / 2)
+                    {
+                        return key;
+                    }
+            }
+
+            return default(int);
+        }
+    }
+}
