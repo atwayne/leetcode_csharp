8dc25278f6ed9d11893e6f50e6ccffa7bdc435c6
Wayne Wang <lastr2d2@gmail.com>
2015-06-30 14:49:40 +0800

    229

---
+++ Solutions/Solutions.NUnitTests/Tests/Problem229Test.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Solutions.Problems;
+
+namespace Solutions.NUnitTests.Tests
+{
+    [TestFixture()]
+    public class Problem229Test
+    {
+        [Test()]
+        public void MajorityElementTest()
+        {
+            var input = new int[] { 1 };
+            var actual = Problem229.MajorityElement(input);
+            var expected = new int[] { 1 };
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEquivalent(expected, actual);
+
+            input = new int[] { 1, 1, 1, 1 };
+            actual = Problem229.MajorityElement(input);
+            expected = new int[] { 1 };
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEquivalent(expected, actual);
+
+            input = new int[] { 1, 2 };
+            actual = Problem229.MajorityElement(input);
+            expected = new int[] { 1, 2 };
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEquivalent(expected, actual);
+
+
+            input = new int[] { 1, 2, 3 };
+            actual = Problem229.MajorityElement(input);
+            expected = new int[] { };
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEquivalent(expected, actual);
+
+            input = new int[] { 1, 2, 1, 2 };
+            actual = Problem229.MajorityElement(input);
+            expected = new int[] { 1, 2 };
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEquivalent(expected, actual);
+
+            input = new int[] { 2, 2, 1, 3 };
+            actual = Problem229.MajorityElement(input);
+            expected = new int[] { 2 };
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEquivalent(expected, actual);
+
+            input = new int[] { 3, 2, 3 };
+            actual = Problem229.MajorityElement(input);
+            expected = new int[] { 3 };
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem229.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/majority-element-ii/
+    /// 
+    /// Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
+    /// </summary>
+    public class Problem229
+    {
+        public static IList<int> MajorityElement(int[] nums)
+        {
+            var majority = 0;
+            var secondMajority = 0;
+
+            var counter = 0;
+            var secondCounter = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var number = nums[i];
+                if (majority == number)
+                {
+                    counter++;
+                }
+                else if (secondMajority == number)
+                {
+                    secondCounter++;
+                }
+                else if (counter == 0)
+                {
+                    counter = 1;
+                    majority = number;
+                }
+                else if (secondCounter == 0)
+                {
+                    secondCounter = 1;
+                    secondMajority = number;
+                }
+                else
+                {
+                    counter--;
+                    secondCounter--;
+                }
+            }
+
+            counter = 0;
+            secondCounter = 0;
+            // reset counter
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (majority == nums[i])
+                {
+                    counter++;
+                }
+                else if (secondMajority == nums[i])
+                {
+                    secondCounter++;
+                }
+            }
+            var result = new List<int>();
+            var baseline = nums.Length / 3;
+            if (counter > baseline)
+                result.Add(majority);
+            if (secondCounter > baseline)
+                result.Add(secondMajority);
+
+            return result;
+        }
+    }
+}
