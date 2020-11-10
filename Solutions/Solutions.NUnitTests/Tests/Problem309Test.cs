2a4e8320c7b9cc2de0cc879beef6ce85be9d0fc8
Wayne Wang <nkgailei@gmail.com>
2020-11-10 14:51:06 +0800

    309

---
+++ Solutions/Solutions.NUnitTests/Tests/Problem309Test.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using Solutions.Problems;
+
+namespace Solutions.NUnitTests.Tests
+{
+    [TestFixture]
+    public class Problem1309Test
+    {
+        [Test]
+        public void MaxProfitTest1()
+        {
+            var input = new int[] { 1, 2, 3, 0, 2 };
+            var result = Problem309.MaxProfit(input);
+            var expected = 3;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void MaxProfitTest2()
+        {
+            var input = new int[0];
+            var result = Problem309.MaxProfit(input);
+            var expected = 0;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void MaxProfitTest3()
+        {
+            var input = new int[] { 1 };
+            var result = Problem309.MaxProfit(input);
+            var expected = 0;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void MaxProfitTest4()
+        {
+            var input = new int[] { 2, 1, 4 };
+            var result = Problem309.MaxProfit(input);
+            var expected = 3;
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
---
+++ Solutions/Solutions/Problems/Problem309.cs
@@ -0,0 +1,48 @@
+﻿namespace Solutions.Problems
+{
+    /// <summary>
+    /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+    /// </summary>
+    /// Say you have an array for which the ith element is the price of a given stock on day i.
+    /// Design an algorithm to find the maximum profit.You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+    /// You may not engage in multiple transactions at the same time(ie, you must sell the stock before you buy again).
+    /// After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+    /// 
+    public class Problem309
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            if (prices.Length < 2)
+            {
+                return 0;
+            }
+
+            var days = prices.Length;
+            var dp = new int[days, 2];
+
+            // day 1
+            dp[0, 0] = 0;
+            dp[0, 1] = prices[0] * -1;
+
+            for (var i = 1; i < days; i++)
+            {
+                dp[i, 0] = Max(dp[i - 1, 0], dp[i - 1, 1] + prices[i]);
+                if (i > 1)
+                {
+                    dp[i, 1] = Max(dp[i - 1, 1], dp[i - 2, 0] - prices[i]);
+                }
+                else
+                {
+                    dp[i, 1] = Max(dp[i - 1, 1], dp[i - 1, 0] - prices[i]);
+                }
+            }
+
+            return dp[days - 1, 0];
+        }
+
+        private static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
